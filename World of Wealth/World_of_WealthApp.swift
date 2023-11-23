c36162bb89ba83705cb2d331c7fe529d549a4cdb
Brett Rosen <Brejuro23@gmail.com>
2023-11-23 01:06:42 -0500

    Initial Commit

---
+++ World of Wealth/World_of_WealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_of_WealthApp.swift
+//  World of Wealth
+//
+//  Created by Brett Rosen on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct World_of_WealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ World of WealthUITests/World_of_WealthUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  World_of_WealthUITestsLaunchTests.swift
+//  World of WealthUITests
+//
+//  Created by Brett Rosen on 11/23/23.
+//
+
+import XCTest
+
+final class World_of_WealthUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
