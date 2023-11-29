5ebe0f8831cf48ca9ee70e3ecd65ae782479c9f0
Brett Rosen <Brejuro23@gmail.com>
2023-11-28 23:10:03 -0500

    Attempt at adding pre script

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  World of Wealth
+#
+#  Created by Brett Rosen on 11/28/23.
+#  
+
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
