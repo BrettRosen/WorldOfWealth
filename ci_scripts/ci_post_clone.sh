9512c493b24224e997511e354faad072559bc6b3
Brett Rosen <Brejuro23@gmail.com>
2023-11-28 22:50:39 -0500

    Added post clone script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  World of Wealth
+#
+#  Created by Brett Rosen on 11/28/23.
+#  
+
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
